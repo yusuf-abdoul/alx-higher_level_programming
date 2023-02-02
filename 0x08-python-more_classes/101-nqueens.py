db118aaf001bb151c7bee72629f86595f5dfa489
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2023-02-02 03:10:25 +0000

    code fix

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -129,4 +129,8 @@ if __name__ == "__main__":
         print("N must be at least 4")
         sys.exit(1)
 
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
 
