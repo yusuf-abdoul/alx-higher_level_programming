b6c8853a2366860848a4a79597266ca29858f688
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2023-02-08 14:16:10 +0000

    7-base_geometry.py added

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+"""BaseGeometry class module"""
+
+
 class BaseGeometry:
-    """ Class that defines the attributes of Geometric Shapes """
+    """Geomerty utility class"""
 
     def area(self):
-        """ Method that defines the area of a geomtric shape """
-
+        """Returns the area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Method that recieves the value property
-        Args:
-            name: name of the object
-            value: value of the property
-        """
+        """Validates value"""
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
