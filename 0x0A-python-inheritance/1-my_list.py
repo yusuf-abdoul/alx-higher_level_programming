d49d4d01dc3747077d24273c529c427eee6a2efd
yusuf-abdoul <abdulhayyiyusuf2015@gmail.com>
2023-02-08 12:27:20 +0000

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """The ``MyList`` module"""
 
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-    Args:
-        list: class list
-    """
+    """MyList inheriting from the ``list`` class"""
 
     def print_sorted(self):
         """ Method that prints the sorted list """
